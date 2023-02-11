05a72008f27a14ea65dc59e0348aca4c0a2b009e
Michel Braga <31113222+mbraga2023@users.noreply.github.com>
2023-02-11 11:51:28 -0300

    week6

--- bank/bank.py
+++
@@ -1,8 +0,0 @@
-answer = input("Greeting: ")
-if answer[0:5] == "Hello":
-    print ("$0")
-elif answer != "Hello":
-    if answer[0] == "H":
-        print ("$20")
-    else:
-        print ("$100")
